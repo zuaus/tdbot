34f26bc54986564ddf300dce93394d92e7867f95
zuaus <30482065+zuaus@users.noreply.github.com>
2019-03-07 23:57:58 +0300

    Update ru.sh

--- ru.sh
+++ ru.sh
@@ -6,26 +6,7 @@ cd $THIS_DIR
 
 install() {
 
-	    cd tdbot		sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-
-		sudo apt-get install g++-4.7 -y c++-4.7 -y
-
-		sudo apt-get update
-
-		sudo apt-get upgrade
-
-		sudo apt-get install libreadline-dev -y libconfig-dev -y libssl-dev -y lua5.2 -y liblua5.2-dev -y lua-socket -y lua-sec -y lua-expat -y libevent-dev -y make unzip git redis-server autoconf g++ -y libjansson-dev -y libpython-dev -y expat libexpat1-dev -y
-
-		sudo apt-get install screen -y
-
-		sudo apt-get install tmux -y
-
-		sudo apt-get install libstdc++6 -y
-
-		sudo apt-get install lua-lgi -y
-
-		sudo apt-get install libnotify-dev -y
-
+	 
 		wget https://valtman.name/files/telegram-bot-180116-nightly-linux
 
 		mv telegram-bot-180116-nightly-linux tg
@@ -124,7 +105,7 @@ default_profile = "cli";
 
 cli = {
 
-lua_script = "$HOME/tdbot/bot.lua";
+lua_script = "$HOME/tdbot/bot/bot.lua";
 
 };
 
