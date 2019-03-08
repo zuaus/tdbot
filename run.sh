7bd8ed60b79e0180ffa5993cfe519eb2511c91e2
zuaus <30482065+zuaus@users.noreply.github.com>
2019-03-08 17:14:44 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -69,6 +69,7 @@ wget https://valtman.name/files/telegram-bot-180116-nightly-linux
 mv telegram-bot-180116-nightly-linux tg
 
 chmod +x tg
+echo -e "\e[1m\e[32m==> \e[97m The install good .. Telegram-Bot\e[0m"
 
 }
 
