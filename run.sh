7c5f0eb4edb0be0d13e19b4fbf09c35c6bb7e3f6
zuaus <30482065+zuaus@users.noreply.github.com>
2019-03-08 22:48:43 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ cd $THIS_DIR
 STORMCLI() {
 ./tg -p cli --login --phone=${1}
 } 
-if [ "$1" = "config" ]; then
+case $1 in
+config)
 echo -e "\e[1;35m ==>[ Installation completed successfully ]\e[00;39;40m"
 sleep 1
 mkdir $HOME/.telegram-bot; cat <<EOF > $HOME/.telegram-bot/config
@@ -18,10 +19,11 @@ sleep 1
 echo -e "\e[1;35m ==>[ Please Insert Your Phone Number... ]\e[00;39;40m"
 read phone_number
 STORMCLI ${phone_number}
-sleep 2
-if
+exit;;
+run)
 echo -e "\e[1;35m ==>[ The Bot Is Run  ]\e[00;39;40m"
 ./tg | grep -v "{"
+esac
 exit 0
 
  
